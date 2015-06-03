3148727744f2a8e1186e13f27b02988b91531486
Reto Kaiser <reto@cargomedia.ch>
2015-06-03 17:56:47 +0200

    adjust test

--- tests/source/WebNavigator/NavigatorTest.php
+++ tests/source/WebNavigator/NavigatorTest.php
@@ -7,6 +7,8 @@ use WebNavigator\Navigator;
 class NavigatorTest extends \PHPUnit_Framework_TestCase {
 
     public function testConstruct() {
-        $navigator = new Navigator();
+        $capabilities = new \DesiredCapabilities([\WebDriverCapabilityType::BROWSER_NAME => 'phantomjs']);
+        $driver = \RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+        $navigator = new Navigator($driver, 'http://www.example.com');
     }
 }
