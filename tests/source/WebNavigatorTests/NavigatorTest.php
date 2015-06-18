7fa59aca374b9886a5449194a7ff0fbee791fc66
Reto Kaiser <reto@cargomedia.ch>
2015-06-18 18:52:41 +0200

    Add Travis config
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Debugging
    
    Cleanup travis config

--- tests/source/WebNavigatorTests/NavigatorTest.php
+++ tests/source/WebNavigatorTests/NavigatorTest.php
@@ -9,6 +9,8 @@ class NavigatorTest extends TestCase {
     public function testGetText() {
         $this->_navigator->get('/test1.html');
 
+        var_dump($this->_navigator->getHtml('html'));
+
         $this->assertSame('Hello', $this->_navigator->getText('#id1'));
     }
 }
